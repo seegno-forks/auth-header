881b619cdf2d63afc3348738fbe4a135ba817388
Ricardo Gama <rgama@uphold.com>
2017-06-08 10:03:15 +0100

    Release 0.4.0

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.parse = exports.format = exports.InvalidHeaderError = undefined;
+
+var _invalidHeaderError = require('./invalid-header-error');
+
+var _invalidHeaderError2 = _interopRequireDefault(_invalidHeaderError);
+
+var _format = require('./format');
+
+var _format2 = _interopRequireDefault(_format);
+
+var _parse = require('./parse');
+
+var _parse2 = _interopRequireDefault(_parse);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.InvalidHeaderError = _invalidHeaderError2.default;
+exports.format = _format2.default;
+exports.parse = _parse2.default;
+//# sourceMappingURL=index.js.map
